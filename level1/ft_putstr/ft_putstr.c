9c8e3b58ab360e0e2574f520418a7b9e6ac72444
Flo.D <dely2200@gmail.com>
2016-01-25 15:09:01 +0100

    level1 ft_putstr

---
+++ level1/ft_putstr/ft_putstr.c
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putstr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ndavid <marvin@42.fr>                      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/09/21 18:03:39 by ndavid            #+#    #+#             */
+/*   Updated: 2015/09/21 18:12:12 by ndavid           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void		ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void		ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	while (str[i])
+	{
+		ft_putchar(str[i]);
+		i++;
+	}
+}
+
+int		main(int argc, char **argv)
+{
+	ft_putstr(argv[1]);
+	return (0);
+}
